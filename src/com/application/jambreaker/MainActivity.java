45deff64d4a1b26b81693db81c3f6e733146baa6
Sukrit <sraghuvanshi@paypal.com>
2014-08-23 17:49:47 +0800

    First commit with working map

---
+++ src/com/application/jambreaker/MainActivity.java
@@ -0,0 +1,34 @@
+package com.application.jambreaker;
+ 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.widget.Toast;
+ 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+ 
+public class MainActivity extends FragmentActivity {
+    GoogleMap map;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+        map = ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+        if (map == null) {
+            Toast.makeText(this, "Google Maps not available", 
+                Toast.LENGTH_LONG).show();
+        }
+    }
+ 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is
+        // present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
